a014aa3adbfc5ada8ed5ed44cf7676692638e639
rafiki <mark@rafiki>
2015-04-16 00:25:42 +0200

    I'm renaming io to utils as suggested on Slack, io is a Python builtin, commands like gettempdir() don't work anymore if you have a local io module. Also a new name is appropriate since it isn't just IO

--- validation/score.py
+++ validation/score.py
@@ -3,20 +3,21 @@
 	Functions for assessing the performance of different classifiers.
 """
 
-from numpy import array, all, ones, zeros
+from numpy import array, ones, zeros
 from numpy.random import uniform
-from scipy import clip, log, subtract
-from validation.normalize import rows_normalized, normalize_probabilities
+from scipy import clip, log
+
+from utils.normalize import rows_normalized, normalize_probabilities
 
 
 def logloss(predictions, true_classes, epsilon = 1.e-15):
 	"""
 		The multiclass logarithmic loss function used in the competition, including the truncation.
-		
+
 		:param predictions: An Nx9 ndarray with prediction probabilities. Rows are samples in the same order as true_classes, and columns correspond to classes (from 1 to 9).
 		:param true_classes: A 9 element vector with the correct class labels (integers from 1 to 9).
 		:return: The logloss (float).
-		
+
 		Information: https://www.kaggle.com/c/otto-group-product-classification-challenge/details/evaluation
 		Based on: https://www.kaggle.com/wiki/LogarithmicLoss   but actually changed completely
 	"""
@@ -30,12 +31,12 @@ def logloss(predictions, true_classes, epsilon = 1.e-15):
 def accuracy():
 	"""
 		The accuracy of the predictions.
-		
+
 		Arguments are the same as those for logloss.
 		:return: The accuracy as a fraction [0-1].
 	"""
 	#todo
-	
+
 
 if __name__ == '__main__':
 	S, C = 21, 9
