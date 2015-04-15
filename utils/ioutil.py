78aff25bd8de6f072ed292f14e3d9b1b202aa796
rafiki <mark@rafiki>
2015-04-16 01:23:29 +0200

    I changed the loading to use cache in the systems temp dir, cutting the load time after the first by several orders of magnitude. I kept the gloabs in ioutils in case anyone depended on them.

--- utils/ioutil.py
+++ utils/ioutil.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from utils.loading import get_training_data, get_testing_data
 
 
 global TRAINSIZE, TESTSIZE, NFEATS
@@ -8,56 +9,58 @@ global TRAINSIZE, TESTSIZE, NFEATS
 TRAINSIZE=61878
 TESTSIZE=144368
 NFEATS=93
-
-
-def _read_data(test='data/test.csv', train='data/train.csv'):
-    """
-    read_data('train.csv', 'test.csv')
-
-    Parse the training and test data at the given paths.
-
-    Parameters
-    ----------
-    test :  str
-            the file containing the test data
-            Defaults to '../test' (hard link to data/test.csv)
-    train : str
-            the file containing the training data.
-            Defaults to '../train' (hard link to data/train.csv)
-
-    Returns
-    -------
-    A 3-tuple (ndarray, ndarray, tuple) with the test and training data
-    with its labels.
-    """
-    testfile, trainfile = open(test), open(train)
-    def feats(dat):
-        return np.asarray(dat[:NFEATS], dtype=np.int16)
-
-    def reader(inf):
-        inf.readline()
-        for i, line in enumerate(inf.readlines()):
-            dat = line[:-1].split(',')[1:]
-            yield i, dat
-        inf.close()
-
-    test = np.zeros((TESTSIZE, NFEATS), dtype=np.int16)
-    train = np.zeros((TRAINSIZE, NFEATS), dtype=np.int16)
-    labels = np.zeros(TRAINSIZE, dtype=np.int8)
-
-    for i,dat in reader(testfile):
-        test[i] = feats(dat)
-
-    for i,dat in reader(trainfile):
-        train[i] = feats(dat)
-        labels[i] = np.int8(dat[NFEATS][6])
-
-    return test, train, labels
-
-
+#
+#
+# def _read_data(test='data/test.csv', train='data/train.csv'):
+#     """
+#     read_data('train.csv', 'test.csv')
+#
+#     Parse the training and test data at the given paths.
+#
+#     Parameters
+#     ----------
+#     test :  str
+#             the file containing the test data
+#             Defaults to '../test' (hard link to data/test.csv)
+#     train : str
+#             the file containing the training data.
+#             Defaults to '../train' (hard link to data/train.csv)
+#
+#     Returns
+#     -------
+#     A 3-tuple (ndarray, ndarray, tuple) with the test and training data
+#     with its labels.
+#     """
+#     testfile, trainfile = open(test), open(train)
+#     def feats(dat):
+#         return np.asarray(dat[:NFEATS], dtype=np.int16)
+#
+#     def reader(inf):
+#         inf.readline()
+#         for i, line in enumerate(inf.readlines()):
+#             dat = line[:-1].split(',')[1:]
+#             yield i, dat
+#         inf.close()
+#
+#     test = np.zeros((TESTSIZE, NFEATS), dtype=np.int16)
+#     train = np.zeros((TRAINSIZE, NFEATS), dtype=np.int16)
+#     labels = np.zeros(TRAINSIZE, dtype=np.int8)
+#
+#     for i,dat in reader(testfile):
+#         test[i] = feats(dat)
+#
+#     for i,dat in reader(trainfile):
+#         train[i] = feats(dat)
+#         labels[i] = np.int8(dat[NFEATS][6])
+#
+#     return test, train, labels
+#
+#
 global TEST, TRAIN, LABELS
 
-TEST, TRAIN, LABELS = _read_data()
+TRAIN, _classes, _features = get_training_data()
+LABELS = _classes.values()
+TEST, _features = get_testing_data()
 
 
 submissionheader = 'id,Class_1,Class_2,Class_3,Class_4,Class_5,Class_6,Class_7,Class_8,Class_9'
---
+++ utils/loading.py
@@ -0,0 +1,108 @@
+
+"""
+	Load train and test data.
+
+	Performance for me (Mark):
+	- first load with this method:   9.5s
+	- later loads with this method:  0.3s
+	- any load with previous method: 8.8s
+	So the initial loading can be faster, but there's no need.
+"""
+
+from tempfile import gettempdir
+from numpy import array, uint16, load, save, empty
+from collections import OrderedDict
+from os.path import join
+from settings import VERBOSITY, TRAIN_DATA_PATH, TEST_DATA_PATH
+
+
+TRAINSIZE = 61878
+TESTSIZE = 144368
+NFEATS = 93
+
+
+def load_training_data(filepath = TRAIN_DATA_PATH):
+	"""
+		Load training data.
+
+		:return: data[ndarray], samples:classes[OrderedDict], features[list]
+
+		Notes:
+		* I tried using Pandas to have a dataframe with labels, but it seems I can't skip class label column. -Mark
+		* The samples are numbered from 1 because the data file does that. Sorry.
+	"""
+	with open(filepath, 'r') as fh:
+		cells = [line.split(',') for line in fh.read().splitlines()]
+	features = cells[0][1:-1]
+	classes = OrderedDict()
+	data = empty((TRAINSIZE, NFEATS), dtype = uint16)
+	for k, row in enumerate(cells[1:]):
+		classes[int(row[0])] = uint16(row[-1].split('_')[-1])
+		data[k, :] = row[1:-1]
+	return data, classes, features
+
+
+def load_testing_data(filepath = TEST_DATA_PATH):
+	"""
+		Load testing data.
+
+		:return: data[ndarray], features[list]
+	"""
+	with open(filepath, 'r') as fh:
+		cells = [line.split(',') for line in fh.read().splitlines()]
+	features = cells[0][1:]
+	data = empty((TESTSIZE, NFEATS), dtype = uint16)
+	for k, row in enumerate(cells[1:]):
+		data[k, :] = row[1:]
+	return data, features
+
+
+def get_training_data(filepath = TRAIN_DATA_PATH):
+	"""
+		Gets the training data from the CSV file, caching it in temporary directory for speed.
+	"""
+	try:
+		data = load(join(gettempdir(), 'cache_train_data.npy'))
+		features = load(join(gettempdir(), 'cache_train_features.npy'))
+		classvals = load(join(gettempdir(), 'cache_train_classes.npy'))
+		classes = OrderedDict((k + 1, val) for k, val in enumerate(classvals))
+		if VERBOSITY >= 1:
+			print 'loaded train data from cache in "{0:s}"'.format(gettempdir())
+	except:
+		data, classes, features = load_training_data(filepath = filepath)
+		save(join(gettempdir(), 'cache_train_data.npy'), data)
+		save(join(gettempdir(), 'cache_train_features.npy'), features)
+		save(join(gettempdir(), 'cache_train_classes.npy'), array(classes.values()))
+		if VERBOSITY >= 1:
+			print 'loaded train data directly'
+	return data, classes, features
+
+
+def get_testing_data(filepath = TEST_DATA_PATH):
+	"""
+		Gets the test data from the CSV file, caching it in temporary directory for speed.
+	"""
+	try:
+		data = load(join(gettempdir(), 'cache_test_data.npy'))
+		features = load(join(gettempdir(), 'cache_test_features.npy'))
+		if VERBOSITY >= 1:
+			print 'loaded test data from cache in "{0:s}"'.format(gettempdir())
+	except:
+		data, features = load_testing_data(filepath = filepath)
+		save(join(gettempdir(), 'cache_test_data.npy'), data)
+		save(join(gettempdir(), 'cache_test_features.npy'), features)
+		if VERBOSITY >= 1:
+			print 'loaded test data directly'
+	return data, features
+
+
+"""
+	Sorry for removing the TEST, TRAIN and LABELS globals. It is a bad idea to load train and test data if only one of them might be needed.
+"""
+
+
+if __name__ == '__main__':
+	data, classes, features = get_training_data()
+	data, features = get_testing_data()
+
+
