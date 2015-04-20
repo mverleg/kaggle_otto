192d01e9b4af55ed8770342e9c01eff60caf8cb7
rafiki <mark@rafiki>
2015-04-20 11:20:08 +0200

    sparsity

--- demo/data_visualization.py
+++ demo/data_visualization.py
@@ -1,5 +1,5 @@
 
-from numpy import histogram, prod
+from numpy import histogram, prod, log2
 from matplotlib.pyplot import subplots, show
 from utils.loading import get_training_data
 
@@ -13,9 +13,18 @@ fig1, ax1 = subplots(figsize = (6, 4))
 ax1.bar(range(1, 10), clssizes)
 ax1.set_title('class frequencies')
 
+
+"""
+	Show value distribution / sparsity.
+"""
 vec_data = train_data.reshape((prod(train_data.shape),))
 fig2, ax2 = subplots(figsize = (5, 4))
-ax2.hist(vec_data)
+binned_data = histogram(vec_data, bins = range(400))[0]
+ax2.bar(range(399), log2(1 + binned_data), edgecolor = 'blue')
+ax2.set_ylabel('$log_2(1 + \#)$')
+ax3 = fig2.add_subplot(2, 2, 2)
+ax3.pie(x = [(vec_data == 0).sum(), (vec_data != 0).sum()], explode = [0.15, 0], labels = ['0', '>0'], colors = ['blue', 'red'])
+
 
 if __name__ == '__main__':
 	show()
