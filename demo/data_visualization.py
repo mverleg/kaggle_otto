526546b512e3e8aa6b227bd95383be39ad2eeb3c
rafiki <mark@rafiki>
2015-04-20 10:52:20 +0200

    Small changes

--- demo/data_visualization.py
+++ demo/data_visualization.py
@@ -1,5 +1,5 @@
 
-from numpy import histogram
+from numpy import histogram, prod
 from matplotlib.pyplot import subplots, show
 from utils.loading import get_training_data
 
@@ -9,10 +9,13 @@ from utils.loading import get_training_data
 """
 train_data, classes, features = get_training_data()
 clssizes = histogram(classes, bins = [q + .5 for q in range(10)])[0]
-fig, ax = subplots(figsize = (6, 4))
-ax.bar(range(1, 10), clssizes)
-ax.set_title('class frequencies')
+fig1, ax1 = subplots(figsize = (6, 4))
+ax1.bar(range(1, 10), clssizes)
+ax1.set_title('class frequencies')
 
+vec_data = train_data.reshape((prod(train_data.shape),))
+fig2, ax2 = subplots(figsize = (5, 4))
+ax2.hist(vec_data)
 
 if __name__ == '__main__':
 	show()
--- demo/test_crossvalidate.py
+++ demo/test_crossvalidate.py
@@ -5,7 +5,7 @@ from validation.crossvalidate import SampleCrossValidator
 
 
 train_data, true_classes, features = get_training_data()
-validator = SampleCrossValidator(train_data, true_classes, test_frac = 0.3, use_data_frac = 0.7)
+validator = SampleCrossValidator(train_data, true_classes, test_frac = 0.3, use_data_frac = 0.7, show = True)
 for train, classes, test in validator.yield_cross_validation_sets(rounds = 13):
 	prediction = get_random_probabilities(sample_count = test.shape[0])
 	validator.add_prediction(prediction)
