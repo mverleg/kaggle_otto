29d7371048b48c39d10055ef27439b3f2003acb3
rafiki <mark@rafiki>
2015-04-26 14:48:37 +0200

    Functional, convering neural network for our data

--- nnet/playground/tutorial2.py
+++ nnet/playground/tutorial2.py
@@ -2,19 +2,19 @@
 """
 	http://danielnouri.org/notes/2014/12/17/using-convolutional-neural-nets-to-detect-facial-keypoints-tutorial/
 """
-from lasagne.nonlinearities import softmax
 
+from matplotlib.pyplot import subplots, show
+from numpy import array
+from lasagne.nonlinearities import softmax
 from lasagne import layers
 from lasagne.updates import nesterov_momentum
 from nolearn.lasagne import NeuralNet
-from settings import NCLASSES
-from utils.loading import get_training_data
+from nnet.prepare import prepare_data
+from settings import NCLASSES, VERBOSITY
 from utils.shuffling import shuffle
 
 
-X, y, features = get_training_data()
-print X.shape
-
+X, y, features = prepare_data()
 
 net = NeuralNet(
 	layers = [
@@ -23,24 +23,40 @@ net = NeuralNet(
 		('output', layers.DenseLayer),
 		],
 
-	input_shape = (None, 93),
-	hidden_num_units = 75,
-	output_nonlinearity = softmax,  # output layer uses identity function  #todo
+	input_shape = (128, 93),
+	hidden_num_units = 25,
+	output_nonlinearity = softmax,
 	output_num_units = NCLASSES,
 
 	update = nesterov_momentum,
-	update_learning_rate = 0.0000001,
+	update_learning_rate = 0.0001,
 	update_momentum = 0.9,
 
 	regression = False,
-	max_epochs = 3,
-	verbose = 1,
+	max_epochs = 500,
+	verbose = bool(VERBOSITY),
 )
-
-print [q for q in dir(net) if not q.startswith('_')]
 
 B, j, key = shuffle(data = X, classes = y)
 j -= 1
-net.fit(B, j)
+out = net.fit(B, j)
+
+prediction = net.predict(B)
+
+epochs = array([res['epoch'] for res in net.train_history_])
+train_loss = array([res['train_loss'] for res in net.train_history_])
+valid_loss = array([res['valid_loss'] for res in net.train_history_])
+
+fig, ax = subplots()
+ax.plot(epochs, train_loss, color = 'blue', label = 'train')
+ax.plot(epochs, valid_loss, color = 'green', label = 'test')
+ax.set_xlabel('Log epoch')
+ax.set_ylabel('Loss')
+ax.set_xscale('log')
+ax.legend()
+
+
+if __name__ == '__main__':
+	show()
 
 
--- nnet/prepare.py
+++ nnet/prepare.py
@@ -4,15 +4,18 @@
 """
 
 from matplotlib.pyplot import subplots, show
-from numpy import log2, log10
+from numpy import log10
 from utils.loading import get_training_data
 
 
-train_data, classes, features = get_training_data()
-train_data = log10(1 + train_data)
+def prepare_data():
+	train_data, classes, features = get_training_data()
+	train_data = log10(1 + train_data)
+	return train_data, classes, features
 
 
 if __name__ == '__main__':
+	train_data, classes, features = prepare_data()
 	print 'min  ', train_data.min()
 	print 'max  ', train_data.max()
 	print 'std  ', train_data.std()
