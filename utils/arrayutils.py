fec1a4b3bcccf7ff9710cb0da5e691e00dfa3e17
Fenno Vermeij <fennovj@gmail.com>
2015-04-26 16:51:04 +0200

    added some documentation to arrayutils.py

--- utils/arrayutils.py
+++ utils/arrayutils.py
@@ -1,8 +1,24 @@
 import numpy as np
 
 def stack_predictions(predictionmatrix2d, Q):
+    """
+    Transforms a NxQC matrix into a QxNxC matrix, by stacking the slices 
+        across axis 0. The inverse of unstack_predictions
+    :param predictionmatrix2d: The NxQC matrix
+    :param Q: Q, needed to split the matrix correctly.
+    """
    return np.array(np.hsplit(predictionmatrix2d, Q))
    
-def unstack_predictions(predictionmatrix3d):
+def unstack_predictions(predictionmatrix3d, retQ = False):
+    """
+    Transforms a QxNxC matrix into a NxQC matrix, by horizontally stacking the matrices 
+        that were originally stacked across axis 0. The inverse of stack_predictions
+    :param predictionmatrix3d: The QxNxC matrix
+    :param retQ: Whether or not to return Q as well, False by default
+    :return: NxQC matrix obtained by unstacking the input matrix
+    """
     Q, _, _ = np.shape(predictionmatrix3d)
-    return np.hstack(np.squeeze(np.vsplit(predictionmatrix3d,Q)))
+    unstacked = np.hstack(np.squeeze(np.vsplit(predictionmatrix3d,Q)))
+    if retQ:
+        return unstacked, Q
+    return unstacked
