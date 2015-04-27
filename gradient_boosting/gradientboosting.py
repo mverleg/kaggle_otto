46756850b0b178677467819a4fb3d5b50569f3ec
Fenno Vermeij <fennovermeij@student.ru.nl>
2015-04-27 22:01:45 +0200

    Fixed stupid typo in gradient boosting, improved testing code

--- gradient_boosting/gradientboosting.py
+++ gradient_boosting/gradientboosting.py
@@ -37,7 +37,7 @@ def gradientBoosting(train, labels, test, n_estimators = 50, max_depth = 5, cali
     else:      
         model.fit(train[:trainrows, :], labels[:trainrows])
         calibratedmodel = CalibratedClassifierCV(model, calibrationmethod, "prefit")
-        calibratedmodel.fit(train[trainrows:,:], labels[trainrows,:])
+        calibratedmodel.fit(train[trainrows:,:], labels[trainrows:])
     return calibratedmodel.predict_proba(test)    
 
 
@@ -46,10 +46,11 @@ if __name__ == '__main__':
     from utils.loading import get_training_data
     from validation.crossvalidate import SampleCrossValidator
 
-    train_data, true_classes, features = get_training_data()
-    validator = SampleCrossValidator(train_data, true_classes, rounds=2, test_frac=0.3, use_data_frac=0.7)
+    train_data, true_classes, _ = get_training_data()    
+    validator = SampleCrossValidator(train_data, true_classes, rounds=1, test_frac=0.1, use_data_frac=1.0)
     for train, classes, test in validator.yield_cross_validation_sets():
-        prediction = gradientBoosting(train, classes, test, 5, verbose = 0)
+        prediction = gradientBoosting(train, classes, test, 100, 10, 0.1, verbose = 1)
+        #Warning: this will take a while, for faster testing, change the 100 iterations to 1 or something
         validator.add_prediction(prediction)
     validator.print_results()
 
