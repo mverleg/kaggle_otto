219245572bf4e1c43d6fb2b973c3d238a1beaa44
mulan <mark@mulan>
2015-05-11 16:29:42 +0800

    New method of rescaling to prior probabilities

--- utils/postprocess.py
+++ utils/postprocess.py
@@ -1,25 +1,46 @@
 
 """
-    Functions for postprocessing the predictions after they are made
+	Functions for postprocessing the predictions after they are made
 """
 
 import numpy as np
+from numpy import log, ones
+from sklearn.metrics import mean_squared_error
+from scipy.optimize import minimize
 from utils.normalize import normalize_probabilities
 
 def rescale_prior(predictionmatrix, priorprobs):
-    """
-    Rescales predictions to make them match the prior probability
-    :param predictionmatrix: a Nx9 matrix of predictions
-    :param priorprobs: a length-9 vector with prior probabilities, does not need to be normalized,
-        so can just be a bincount of the true_class vector
-    :return: A prediction matrix with rescaled probabilities
-    """
-    priorprobs = np.trim_zeros(priorprobs, 'f')
-    N, C = np.shape(predictionmatrix)
-    assert C == np.size(priorprobs)
-    priorprobs = priorprobs / np.sum(priorprobs).astype(float)
-    averageprediction = np.mean(predictionmatrix, axis = 0)
-    factor = priorprobs / averageprediction
-    result = predictionmatrix * np.repeat(factor[None,:],N,0)
-    return normalize_probabilities(result)
-    
+	"""
+	Rescales predictions to make them match the prior probability
+	:param predictionmatrix: a Nx9 matrix of predictions
+	:param priorprobs: a length-9 vector with prior probabilities, does not need to be normalized,
+		so can just be a bincount of the true_class vector
+	:return: A prediction matrix with rescaled probabilities
+	"""
+	priorprobs = np.trim_zeros(priorprobs, 'f')
+	N, C = np.shape(predictionmatrix)
+	assert C == np.size(priorprobs)
+	priorprobs = priorprobs / np.sum(priorprobs).astype(float)
+	averageprediction = np.mean(predictionmatrix, axis = 0)
+	factor = priorprobs / averageprediction
+	result = predictionmatrix * np.repeat(factor[None,:],N,0)
+	return normalize_probabilities(result)
+
+
+def rescale_to_priors(probabilities, priors):
+	"""
+		Uses iterative minimization to find the best scale factors to approach priors as close as possible.
+
+		:param probabilities: Normalized probabilities.
+		:param priors: Prior probability for each dimension (normalized).
+
+		Simply calculating the scale factor is not optimal because the average probabilities change after renormalization.
+	"""
+	assert abs(priors.sum() - 1) < 1e-6, 'Prior probabilities not normalized.'
+	def prior_mismatch(scale, probs, pris):
+		return log(mean_squared_error(normalize_probabilities(scale * probs).mean(0), pris))
+	result = minimize(prior_mismatch, x0 = priors / probabilities.mean(0), method = 'BFGS',
+		args = (probabilities, priors), options = {'maxiter': 1000})
+	return normalize_probabilities(probabilities * result.x)
+
+
