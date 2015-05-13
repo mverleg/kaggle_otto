7d9e3101c1da3f5ca5d3c34c128e0bedc37924db
lilo3 <mark@lilo3>
2015-05-13 15:11:01 +0200

    small changes on server

--- nnet/optimize/outliers.py
+++ nnet/optimize/outliers.py
@@ -4,7 +4,8 @@ from nnet.base_optimize import optimize_NN
 
 optimize_NN(debug = False, **{
 	'outlier_method': ['OCSVM', 'EE'],
-	'outlier_frac': [0, 0.2, 0.5, 0.12, 0.2],
+	'outlier_frac': [0, 0.02, 0.5, 0.12, 0.2],
+	'rounds': 3,
 })
 
 
