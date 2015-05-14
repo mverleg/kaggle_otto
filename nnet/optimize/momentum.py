845a3e29f2624303755ca239cd8d2a86f3bddb84
mulan <mark@mulan>
2015-05-14 09:18:33 +0800

    Momentum fix

--- nnet/optimize/momentum.py
+++ nnet/optimize/momentum.py
@@ -2,11 +2,11 @@
 from nnet.base_optimize import optimize_NN
 
 
-optimize_NN(debug = False, **{
+optimize_NN(debug = True, **{
 	'dense1_size': 256,
 	'dense2_size': 128,
 	'learning_rate': 0.0001,
-	'momentum': [0, 0.9, 0.99],
+	'momentum': [0.1, 0.9, 0.99, 0.999],
 	'dropout1_rate': 0.5,
 	'rounds': 3,
 })
