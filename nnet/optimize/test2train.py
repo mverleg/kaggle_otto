e9847391e92db0b5b9a1661bbe5d0cecf3ce197c
mulan <mark@mulan>
2015-05-15 10:27:45 +0800

    Try expanding training data for bigger networks

--- nnet/optimize/test2train.py
+++ nnet/optimize/test2train.py
@@ -3,7 +3,11 @@ from nnet.base_optimize import optimize_NN
 
 
 optimize_NN(debug = False, **{
+	'dense1_size': 256,
+	'dense2_size': 128,
+	'dropout1_rate': 00.5,
 	'test_data_confidence': [None, 0.98, 0.95, 0.9, 0.8, 0.6],
+	'rounds': 5,
 })
 
 
