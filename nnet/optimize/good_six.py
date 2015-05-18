dc66498011b48ea3c1829a83a2cfc174c2f0d58e
mulan <mark@mulan>
2015-05-18 14:45:57 +0800

    another try

--- nnet/optimize/good_six.py
+++ nnet/optimize/good_six.py
@@ -7,18 +7,18 @@ optimize_NN(debug = False, **{
 	'dense2_size': 128,
 	'dense3_size': None,
 	'learning_rate': 0.005,
-	'learning_rate_scaling': 100,
-	'momentum': 0.99,
-	'momentum_scaling': 10,
+	'learning_rate_scaling': 5000,
+	'momentum': 0.9,
+	'momentum_scaling': 100,
 	'dropout1_rate': 0.5,
-	'dropout2_rate': 0.5,
+	'dropout2_rate': 0,
 	'dropout3_rate': 0,
 	'weight_decay': 0,
 	'max_epochs': 1500,
 	'extra_feature_count': [0, 163],
 	'pretrain': True,
 	'save_snapshots_stepsize': 1500,
-	'rounds': 5,
+	'rounds': 3,
 })
 
 
